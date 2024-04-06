56cfb21fa0038366f9cc6968d9b81b927bd1b0b2
Eric Muganga <105128548+eric-muganga@users.noreply.github.com>
2024-04-06 14:46:33 +0200

    first commit

---
+++ src/components/Events/EventDetails.jsx
@@ -0,0 +1,153 @@
+import { Link, Outlet, useNavigate, useParams } from "react-router-dom";
+
+import Header from "../Header.jsx";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import { deleteEvent, fetchEvent, queryClient } from "../../util/http.js";
+import LoadingIndicator from "../UI/LoadingIndicator.jsx";
+import ErrorBlock from "../UI/ErrorBlock.jsx";
+import { useState } from "react";
+import Modal from "../UI/Modal.jsx";
+
+export default function EventDetails() {
+  const [isDeleting, setIsDeleting] = useState(false);
+
+  const navigate = useNavigate();
+
+  const { id } = useParams();
+
+  const {
+    data: event,
+    isPending,
+    isError,
+    error,
+  } = useQuery({
+    queryKey: ["events", id],
+    queryFn: ({ signal }) => fetchEvent({ id, signal }),
+  });
+
+  const {
+    mutate,
+    isPending: isDeletePending,
+    isError: isDelError,
+    error: delError,
+  } = useMutation({
+    mutationFn: deleteEvent,
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ["events"],
+        refetchType: "none",
+      });
+      navigate("/events");
+    },
+  });
+
+  function handleDelete() {
+    mutate({ id });
+  }
+
+  function handleStartDelete() {
+    setIsDeleting(true);
+  }
+
+  function handleStopDelete() {
+    setIsDeleting(false);
+  }
+
+  let content;
+
+  if (isPending) {
+    content = (
+      <div id="event-details-content" className="center">
+        <LoadingIndicator />;
+      </div>
+    );
+  }
+
+  if (isError) {
+    content = (
+      <div id="event-details-content" className="center">
+        <ErrorBlock
+          title="An error occurred"
+          message={error.info?.message || "Failed to fetch event"}
+        />
+      </div>
+    );
+  }
+
+  if (event) {
+    const formattedDate = new Date(event.date).toLocaleDateString("en-PL", {
+      day: "numeric",
+      month: "short",
+      year: "numeric",
+    });
+    content = (
+      <>
+        <header>
+          <h1>{event.title}</h1>
+          <nav>
+            {!isDeletePending && (
+              <button onClick={handleStartDelete}>Delete</button>
+            )}
+
+            <Link to="edit">Edit</Link>
+          </nav>
+        </header>
+        <div id="event-details-content">
+          <img src={`http://localhost:3000/${event.image}`} alt={event.title} />
+          <div id="event-details-info">
+            <div>
+              <p id="event-details-location">{event.location}</p>
+              <time dateTime={`Todo-DateT$Todo-Time`}>
+                {formattedDate} @ {event.time}
+              </time>
+            </div>
+            <p id="event-details-description">{event.description}</p>
+          </div>
+        </div>
+      </>
+    );
+  }
+
+  return (
+    <>
+      {isDeleting && (
+        <Modal onClose={handleStopDelete}>
+          <h2>Are you sure?</h2>
+          <p>
+            Do you really want to delete this event? This action cannot be
+            undone
+          </p>
+          <div className="form-actions">
+            {isDeletePending && "Deleting..."}
+            {!isDeletePending && (
+              <>
+                <button onClick={handleStopDelete} className="button-text">
+                  Cancel
+                </button>
+                <button onClick={handleDelete} className="button">
+                  Delete
+                </button>
+              </>
+            )}
+          </div>
+          {isDelError && (
+            <ErrorBlock
+              title="Failed to create event"
+              message={
+                delError.info?.message ||
+                "Failed to create event. Please check your inputs and try again later."
+              }
+            />
+          )}
+        </Modal>
+      )}
+      <Outlet />
+      <Header>
+        <Link to="/events" className="nav-item">
+          View all Events
+        </Link>
+      </Header>
+      <article id="event-details">{content}</article>
+    </>
+  );
+}
